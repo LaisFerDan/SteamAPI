b6064175d2fdaa2d09d2c04f876d7682a5361698
Laís Fernandes Daniel <laisfd@hotmail.com>
2022-09-04 18:30:14 -0300

    Created Dto classes: - GamesDto; -GamesPatchDto.
    Added Post, Patch and Delete methods on GamesController.
    
    Modified UpdateGamesModel class (parameter: from Games entity to GamesDto entity)

--- Controllers/GamesController.cs
+++ Controllers/GamesController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using SteamAPI.Dto;
 using SteamAPI.Interfaces;
 using SteamAPI.Models;
 using System.Net.Mime;
@@ -9,14 +10,15 @@ namespace SteamAPI.Controllers
     [ApiController]
     public class GamesController : ControllerBase
     {
+        private readonly ILogger<GamesController> _logger;
         private readonly IBaseRepository<Games> _repository;
-
-        public GamesController(IBaseRepository<Games> repository)
+        public GamesController(ILogger<GamesController> logger, IBaseRepository<Games> repository)
         {
+            _logger = logger;
             _repository = repository;
         }
 
-        private Games UpdateGamesModel(Games newData, Games entity)
+        private Games UpdateGamesModel(Games newData, GamesDto entity)
         {
             newData.AppId = entity.AppId;
             newData.Name = entity.Name;
@@ -56,17 +58,51 @@ namespace SteamAPI.Controllers
         [ProducesResponseType(typeof(Games), StatusCodes.Status201Created)]
         [ProducesResponseType(typeof(Games), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(string), StatusCodes.Status415UnsupportedMediaType)]
-        public async Task<IActionResult> Put([FromRoute] int id, [FromBody] Games entity)
+        public async Task<IActionResult> Put([FromRoute] int id, [FromBody] GamesDto entity)
         {
             var databaseGames = await _repository.GetByKey(id);
             if (databaseGames == null)
             {
-                var inserted = await _repository.Insert(entity);
+                var gamesToInsert = new Games(id: 0, entity.AppId, entity.Name, entity.Developer, entity.Platforms, entity.Categories, entity.Genres);
+                var inserted = await _repository.Insert(gamesToInsert);
                 return Created(string.Empty, inserted);
             }
             databaseGames = UpdateGamesModel(databaseGames, entity);
             var updated = await _repository.Update(id, databaseGames);
             return Ok(updated);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]GamesDto entity)
+        {
+            var gamesToInsert = new Games(id: 0, entity.AppId, entity.Name, entity.Developer, entity.Platforms, entity.Categories, entity.Genres);
+            var inserted = await _repository.Insert(gamesToInsert);
+            return Created(string.Empty, gamesToInsert);
+        }
+
+        [HttpPatch("{id}")]
+        public async Task<IActionResult> Patch([FromRoute] int id, [FromBody] GamesPatchDto entity)
+        {
+            var databaseGames = await _repository.GetByKey(id);
+
+            if (databaseGames == null)
+                return NoContent();
+
+            databaseGames.Platforms = entity.Platforms;
+            var updated = await _repository.Update(id, databaseGames);
+            return Ok(updated);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            var databaseGames = await _repository.GetByKey(id);
+
+            if (databaseGames == null)
+                return NoContent();
+
+            var deleted = await _repository.Delete(id);
+            return Ok(deleted);
         }
     }
 }
